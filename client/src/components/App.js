122d9579afc19364eaf2f44f16619f211351c0ab
Joma Ejercito <jomaejercito@gmail.com>
2019-10-04 22:15:27 -0400

    Add route paths in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamList from './streams/StreamList';
 import StreamCreate from './streams/StreamCreate';
+import StreamShow from './streams/StreamShow';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
-import StreamList from './streams/StreamList';
-import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
     <div>
-      App
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/show" exact component={StreamShow} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+        </div>
+      </BrowserRouter>
     </div>
   )
 };
