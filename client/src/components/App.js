ef1b4dc3310732bd594f6c571ddc3d1c50555772
Joma Ejercito <jomaejercito@gmail.com>
2019-10-04 22:12:34 -0400

    Add import statements for streams files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
